4e4b1e07a5e7239ac4da4d52453cfae07480b35f
nsochoaga <nsochoaga@unal.edu.co>
2025-04-26 01:11:51 -0500

    test with base to git

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: process.env.DEPLOY_ENV == 'github' ? '/nsochoaga.github.io/' : '',
+  base: '/nsochoaga.github.io/',
   plugins: [react()],
 })
