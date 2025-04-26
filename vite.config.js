0f2c8eb48be88a98bb32504c765db10456df6132
nsochoaga <nsochoaga@unal.edu.co>
2025-04-26 00:52:14 -0500

    fix repository name

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: process.env.DEPLOY_ENV === 'github' ? '/nombre-del-repositorio/' : '',
+  base: process.env.DEPLOY_ENV === 'github' ? '/nsochoaga.github.io/' : '',
   plugins: [react()],
 })
