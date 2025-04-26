ea70e054e7590dc0f00dcc5472780109f348e382
nsochoaga <nsochoaga@unal.edu.co>
2025-04-26 00:47:10 -0500

    test with base if

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '',
+  base: process.env.DEPLOY_ENV === 'github' ? '/nombre-del-repositorio/' : '',
   plugins: [react()],
 })
